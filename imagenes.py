8dea2ade6e54819cb4f1aa0563e16be6269349e5
Pelope9832 <124055188+Pelope9832@users.noreply.github.com>
2023-04-21 18:45:54 +0200

    Deshecho

--- imagenes.py
+++ imagenes.py
@@ -67,8 +67,8 @@ if True:
 
             pipe = StableDiffusionPipeline.from_pretrained(
                 model_id,
-                revision="fp16" if not torch.cuda.is_available() else "fp32",
-                torch_dtype=torch.float16 if not torch.cuda.is_available() else torch.float32, 
+                revision="fp16" if torch.cuda.is_available() else "fp32",
+                torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32, 
                 ).to("cuda")
 
             with st.empty():
